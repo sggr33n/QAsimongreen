6162b9f2d0b1d7101aa6150dd101494aeee69dba
sggr33n <s.g.gr33n@googlemail.com>
2025-09-19 14:45:14 +0100

    Update main.tf
    amend vpc to vpc1

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 resource "google_compute_network" "example_vpc" {
-  name                    = "example-vpc"
+  name                    = "example-vpc1"
   auto_create_subnetworks = true  # Automatically creates subnets for all regions
 }
 
